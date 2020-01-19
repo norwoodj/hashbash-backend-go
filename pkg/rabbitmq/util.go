5922095f9cab4bafa7da9605c6bb90d51f0e5de1
John Norwood <norwood.john.m@gmail.com>
2020-01-19 14:46:28 +0000

    feat: updates to new rabbit client version so that vhost can be configured

--- pkg/rabbitmq/util.go
+++ pkg/rabbitmq/util.go
@@ -19,13 +19,16 @@ func AddRabbitMqFlags(flags *pflag.FlagSet) {
 	flags.String("rabbitmq-username", "guest", "The username with which to authenticate to the rabbitmq metrics")
 	flags.String("rabbitmq-password", "guest", "The password with which to authenticate to the rabbitmq metrics")
 	flags.Int("rabbitmq-port", 5672, "The port on which to connect to the hashbash rabbitmq metrics")
+	flags.String("rabbitmq-vhost", "", "The virtual host to connect to on the rabbitmq server")
 }
 
 func AcquireMqConnectionOrDie() *rabbitmq.ServerConnection {
 	rabbitConfig := rabbitmq.NewConfig(
 		viper.GetString("rabbitmq-host"),
+		viper.GetInt("rabbitmq-port"),
 		viper.GetString("rabbitmq-username"),
 		viper.GetString("rabbitmq-password"),
+		viper.GetString("rabbitmq-vhost"),
 	)
 
 	connection, err := rabbitmq.NewServerConnection(rabbitConfig)
